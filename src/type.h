3dda43373a84e95bd72894f1729e58b278292738
bosswang <377591574@qq.com>
2019-06-15 19:41:54 +0800

    add type.h

---
+++ src/type.h
@@ -0,0 +1,20 @@
+//
+// Created by BossWang & ItemZheng on 2019-06-15.
+//
+
+#ifndef COMPILER_TYPE_H
+#define COMPILER_TYPE_H
+
+#include "stdlib.h"
+
+typedef enum{
+    INT,
+    LONG,
+    FLOAT,
+    SHORT,
+    VOID,
+    DOUBLE,
+    CHAR
+} type_var;
+
+#endif
